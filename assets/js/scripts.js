317ced3e9885d931ffe4ae88b51a03c3098cfde0
jatparam <jatharsan.param@gmail.com>
2017-07-25 23:27:16 -0400

    Initial Commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('ul.tabs').tabs();
+  $('.slider').slider({height: 700});
+  $('.materialboxed').materialbox();
+ });
